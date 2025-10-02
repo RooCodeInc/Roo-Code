1d8061801d3d869baffef0e8f676e2e52656e1f7
Prasang Prajapati <Prasang.A.Prajapati@ibm.com>
2025-10-02 12:19:01 -0400

    update if condition for platform validation and update apikey casing..

--- src/core/webview/webviewMessageHandler.ts
+++ src/core/webview/webviewMessageHandler.ts
@@ -958,9 +958,11 @@ export const webviewMessageHandler = async (
 					const { apiKey, projectId, platform, baseUrl, authType, username, password, region } =
 						message.values
 
-					if (platform !== "ibmCloud") {
-						if (!apiKey && !region && !projectId) {
-							console.error("Missing IBM Cloud authentication credentials for IBM watsonx models")
+					if (platform === "ibmCloud") {
+						if (!apiKey || !region || !projectId) {
+							console.error(
+								"Missing IBM Cloud authentication credentials in IBM watsonx AI provider for IBM watsonx models",
+							)
 							provider.postMessageToWebview({
 								type: "watsonxModels",
 								watsonxModels: {},
@@ -968,20 +970,28 @@ export const webviewMessageHandler = async (
 							return
 						}
 					} else if (platform === "cloudPak") {
-						if (authType === "password" && !(username && password && projectId)) {
-							console.error("Missing IBM Cloud Pak authentication credentials for IBM watsonx models")
-							provider.postMessageToWebview({
-								type: "watsonxModels",
-								watsonxModels: {},
-							})
-							return
-						} else if (authType === "apikey" && !(apiKey && projectId)) {
-							console.error("Missing IBM Cloud Pak authentication credentials for IBM watsonx models")
-							provider.postMessageToWebview({
-								type: "watsonxModels",
-								watsonxModels: {},
-							})
-							return
+						if (authType === "password") {
+							if (!baseUrl || !username || !password || !projectId) {
+								console.error(
+									"Missing IBM Cloud Pak for Data authentication credentials in IBM watsonx AI provider for IBM watsonx models",
+								)
+								provider.postMessageToWebview({
+									type: "watsonxModels",
+									watsonxModels: {},
+								})
+								return
+							}
+						} else if (authType === "apiKey") {
+							if (!baseUrl || !apiKey || !username || !projectId) {
+								console.error(
+									"Missing IBM Cloud Pak for Data authentication credentials in IBM watsonx AI provider for IBM watsonx models",
+								)
+								provider.postMessageToWebview({
+									type: "watsonxModels",
+									watsonxModels: {},
+								})
+								return
+							}
 						}
 					}
 
@@ -996,7 +1006,7 @@ export const webviewMessageHandler = async (
 							"ca-tor": "https://ca-tor.ml.cloud.ibm.com",
 							"ap-south-1": "https://ap-south-1.aws.wxai.ibm.com",
 						}
-						effectiveBaseUrl = regionToUrl[region] || "https://us-south.ml.cloud.ibm.com"
+						effectiveBaseUrl = regionToUrl[region]
 					}
 
 					const watsonxModels = await getWatsonxModels(
