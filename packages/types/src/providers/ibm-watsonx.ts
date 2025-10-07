54c24b6c54f41ec557f0ceefbf13a3d9d87513bc
Prasang Prajapati <Prasang.A.Prajapati@ibm.com>
2025-10-07 12:19:48 -0400

    remove REGION_TO_URL duplication and fix event handler

--- packages/types/src/providers/ibm-watsonx.ts
+++ packages/types/src/providers/ibm-watsonx.ts
@@ -1,5 +1,15 @@
 import type { ModelInfo } from "../model.js"
 
+export const REGION_TO_URL: Record<string, string> = {
+	"us-south": "https://us-south.ml.cloud.ibm.com",
+	"eu-de": "https://eu-de.ml.cloud.ibm.com",
+	"eu-gb": "https://eu-gb.ml.cloud.ibm.com",
+	"jp-tok": "https://jp-tok.ml.cloud.ibm.com",
+	"au-syd": "https://au-syd.ml.cloud.ibm.com",
+	"ca-tor": "https://ca-tor.ml.cloud.ibm.com",
+	"ap-south-1": "https://ap-south-1.aws.wxai.ibm.com",
+}
+
 export type WatsonxAIModelId = keyof typeof watsonxModels
 export const watsonxDefaultModelId = "ibm/granite-3-3-8b-instruct"
 
