ae2ea0f53d567d56b7f6263b75cf32c1ffa164bc
Jakob Malmo <jakobmalmo@protonmail.com>
2025-10-16 20:20:01 +0200

    fix: Confirmed the follow-up styling tweak is in place: chat titles now render without font-semibold

--- webview-ui/src/components/history/TaskItem.tsx
+++ webview-ui/src/components/history/TaskItem.tsx
@@ -86,12 +86,9 @@ const TaskItem = ({
 						)}
 						data-testid="task-content">
 						{displayHighlight ? (
-							<span
-								className={cn(showTitle && "font-semibold")}
-								dangerouslySetInnerHTML={{ __html: displayHighlight }}
-							/>
+							<span dangerouslySetInnerHTML={{ __html: displayHighlight }} />
 						) : (
-							<span className={cn(showTitle && "font-semibold")}>{displayText}</span>
+							<span>{displayText}</span>
 						)}
 					</div>
 
