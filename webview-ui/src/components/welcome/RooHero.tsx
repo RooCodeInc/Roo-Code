cce8a83737cdfa8167ec75b2bfdbe31904038669
Bruno Bergher <me@brunobergher.com>
2025-11-06 11:58:08 +0000

    Update webview-ui/src/components/welcome/RooHero.tsx
    Co-authored-by: roomote[bot] <219738659+roomote[bot]@users.noreply.github.com>

--- webview-ui/src/components/welcome/RooHero.tsx
+++ webview-ui/src/components/welcome/RooHero.tsx
@@ -19,7 +19,7 @@ const RooHero = () => {
 					maskSize: "contain",
 				}}
 				className="mx-auto hover:animate-bounce">
-				<img src={imagesBaseUri + "/roo-logo.svg"} alt="Roo logo" className="h-8 opacity-0 " />
+				<img src={imagesBaseUri + "/roo-logo.svg"} alt="Roo logo" className="h-8 opacity-0" />
 			</div>
 		</div>
 	)
