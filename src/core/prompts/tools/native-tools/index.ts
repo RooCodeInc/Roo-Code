9154b7219489b11c7da8ba6dc5e1707530873ea0
daniel-lxs <ricciodaniel98@gmail.com>
2025-11-13 10:58:50 -0500

    Remove unimplemented tools (search_and_replace, edit_file)

--- src/core/prompts/tools/native-tools/index.ts
+++ src/core/prompts/tools/native-tools/index.ts
@@ -3,7 +3,6 @@ import askFollowupQuestion from "./ask_followup_question"
 import attemptCompletion from "./attempt_completion"
 import browserAction from "./browser_action"
 import codebaseSearch from "./codebase_search"
-import editFile from "./edit_file"
 import executeCommand from "./execute_command"
 import fetchInstructions from "./fetch_instructions"
 import generateImage from "./generate_image"
@@ -13,7 +12,6 @@ import listFiles from "./list_files"
 import newTask from "./new_task"
 import { read_file } from "./read_file"
 import runSlashCommand from "./run_slash_command"
-import searchAndReplace from "./search_and_replace"
 import searchFiles from "./search_files"
 import switchMode from "./switch_mode"
 import updateTodoList from "./update_todo_list"
@@ -29,7 +27,6 @@ export const nativeTools = [
 	attemptCompletion,
 	browserAction,
 	codebaseSearch,
-	editFile,
 	executeCommand,
 	fetchInstructions,
 	generateImage,
@@ -39,7 +36,6 @@ export const nativeTools = [
 	newTask,
 	read_file,
 	runSlashCommand,
-	searchAndReplace,
 	searchFiles,
 	switchMode,
 	updateTodoList,
