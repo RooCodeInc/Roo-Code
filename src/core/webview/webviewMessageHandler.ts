db524c00c865b00e1a170d807d443cf4376c3c13
Hannes Rudolph <hrudolph@gmail.com>
2026-01-09 15:17:10 -0700

    fix: remove redundant postStateToWebview call after debug setting update

--- src/core/webview/webviewMessageHandler.ts
+++ src/core/webview/webviewMessageHandler.ts
@@ -2303,7 +2303,6 @@ export const webviewMessageHandler = async (
 			await vscode.workspace
 				.getConfiguration(Package.name)
 				.update("debug", message.bool ?? false, vscode.ConfigurationTarget.Global)
-			await provider.postStateToWebview()
 			break
 		}
 		case "cloudButtonClicked": {
