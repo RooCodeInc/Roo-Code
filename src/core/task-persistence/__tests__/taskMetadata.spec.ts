b2fed26c6fe21201f2d8774573c027a111fa4097
Toray Altas <toray.altas@gmail.com>
2026-01-21 09:43:38 -0500

    test: remove obsolete taskMetadata spec file

--- src/core/task-persistence/__tests__/taskMetadata.spec.ts
+++
@@ -1,3 +0,0 @@
-// This file previously tested line change tracking in taskMetadata,
-// which has been removed as part of eliminating line change tracking.
-// The file is kept as a placeholder for future taskMetadata tests.
