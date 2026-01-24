2b1e10c5301781997964c62113b2afe75e4e2555
Hannes Rudolph <hrudolph@gmail.com>
2026-01-23 20:17:06 -0700

    fix: openSettings message passes section in values object
    The webviewMessageHandler was passing section as 'tab' property but
    App.tsx expects it in 'values.section'. This fixes the navigation from
    CodeIndexPopover 'Configure in Settings' link to the indexing tab.

--- src/core/webview/webviewMessageHandler.ts
+++ src/core/webview/webviewMessageHandler.ts
@@ -2857,7 +2857,7 @@ export const webviewMessageHandler = async (
 			provider.postMessageToWebview({
 				type: "action",
 				action: "settingsButtonClicked",
-				tab: message.section,
+				values: { section: message.section },
 			})
 			break
 		}
